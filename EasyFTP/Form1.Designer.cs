9b3c3ee1d9123230c562dfe982b37eeb52d80daf
Cristian Alonso Vallejo <cr.alonsov@gmail.com>
2017-08-07 13:07:23 +0200

    Add project files.

---
+++ EasyFTP/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace EasyFTP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.label_ip = new System.Windows.Forms.Label();
+            this.label_user = new System.Windows.Forms.Label();
+            this.textBox_user = new System.Windows.Forms.TextBox();
+            this.label_pass = new System.Windows.Forms.Label();
+            this.textBox_pass = new System.Windows.Forms.TextBox();
+            this.button_showFiles = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(12, 51);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(163, 20);
+            this.textBox_ip.TabIndex = 0;
+            // 
+            // label_ip
+            // 
+            this.label_ip.AutoSize = true;
+            this.label_ip.Location = new System.Drawing.Point(12, 35);
+            this.label_ip.Name = "label_ip";
+            this.label_ip.Size = new System.Drawing.Size(58, 13);
+            this.label_ip.TabIndex = 1;
+            this.label_ip.Text = "IP Address";
+            // 
+            // label_user
+            // 
+            this.label_user.AutoSize = true;
+            this.label_user.Location = new System.Drawing.Point(12, 88);
+            this.label_user.Name = "label_user";
+            this.label_user.Size = new System.Drawing.Size(55, 13);
+            this.label_user.TabIndex = 3;
+            this.label_user.Text = "Username";
+            // 
+            // textBox_user
+            // 
+            this.textBox_user.Location = new System.Drawing.Point(12, 104);
+            this.textBox_user.Name = "textBox_user";
+            this.textBox_user.Size = new System.Drawing.Size(163, 20);
+            this.textBox_user.TabIndex = 2;
+            // 
+            // label_pass
+            // 
+            this.label_pass.AutoSize = true;
+            this.label_pass.Location = new System.Drawing.Point(12, 146);
+            this.label_pass.Name = "label_pass";
+            this.label_pass.Size = new System.Drawing.Size(53, 13);
+            this.label_pass.TabIndex = 5;
+            this.label_pass.Text = "Password";
+            // 
+            // textBox_pass
+            // 
+            this.textBox_pass.Location = new System.Drawing.Point(12, 162);
+            this.textBox_pass.Name = "textBox_pass";
+            this.textBox_pass.PasswordChar = '●';
+            this.textBox_pass.Size = new System.Drawing.Size(163, 20);
+            this.textBox_pass.TabIndex = 4;
+            // 
+            // button_showFiles
+            // 
+            this.button_showFiles.Location = new System.Drawing.Point(12, 209);
+            this.button_showFiles.Name = "button_showFiles";
+            this.button_showFiles.Size = new System.Drawing.Size(75, 23);
+            this.button_showFiles.TabIndex = 6;
+            this.button_showFiles.Text = "Show Files";
+            this.button_showFiles.UseVisualStyleBackColor = true;
+            this.button_showFiles.Click += new System.EventHandler(this.showFiles);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(249, 51);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(486, 234);
+            this.treeView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 455);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button_showFiles);
+            this.Controls.Add(this.label_pass);
+            this.Controls.Add(this.textBox_pass);
+            this.Controls.Add(this.label_user);
+            this.Controls.Add(this.textBox_user);
+            this.Controls.Add(this.label_ip);
+            this.Controls.Add(this.textBox_ip);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.Label label_ip;
+        private System.Windows.Forms.Label label_user;
+        private System.Windows.Forms.TextBox textBox_user;
+        private System.Windows.Forms.Label label_pass;
+        private System.Windows.Forms.TextBox textBox_pass;
+        private System.Windows.Forms.Button button_showFiles;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ EasyFTP/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyFTP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static FtpWebRequest GetWebRequest(string user, string pass, string uri, string method)
+        {
+            Uri serverUri = new Uri(uri);
+            if (serverUri.Scheme != Uri.UriSchemeFtp)
+            {
+                return null;
+            }
+
+            FtpWebRequest reqFTP;
+            reqFTP = (FtpWebRequest)WebRequest.Create(serverUri);
+            reqFTP.UseBinary = true;
+            reqFTP.Credentials = new NetworkCredential(user, pass);
+            reqFTP.Method = method;
+            reqFTP.Proxy = null;
+            reqFTP.KeepAlive = false;
+            reqFTP.UsePassive = false;
+
+            return reqFTP;
+        }
+
+
+        private static IEnumerable<FtpDetails> GetFileList(string user, string pass, string rootUri, string folder)
+        {
+            var CurrentRemoteDirectory = rootUri;
+            StringBuilder result = new StringBuilder();
+
+            if (!folder.EndsWith("/") && folder.Length > 0)
+                folder += "/";
+
+            FtpWebRequest request = GetWebRequest(user, pass, CurrentRemoteDirectory, WebRequestMethods.Ftp.ListDirectoryDetails);
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    string line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        result.Append(line);
+                        result.Append("\n");
+                        line = reader.ReadLine();
+                    }
+
+                    result.Remove(result.ToString().LastIndexOf('\n'), 1);
+                    var results = result.ToString().Split('\n');
+
+                    var myResult = new List<FtpDetails>();
+                    foreach (var file in results)
+                    {
+                        string[] tokens = file.Split(new[] { ' ' }, 9, System.StringSplitOptions.RemoveEmptyEntries);
+                        string fileTime = tokens[0];
+                        string fileDate = tokens[1];
+                        string fileType = tokens[2];
+                        string fileName = tokens[3];
+
+                        myResult.Add(new FtpDetails()
+                        {
+                            Time = fileTime,
+                            Date = fileDate,
+                            Type = fileType,
+                            Name = fileName,
+                            FullPath = CurrentRemoteDirectory + "/" + fileName,
+                        });
+                    }
+
+                    return myResult;
+                }
+            }
+        }
+
+        private void showFiles(object sender, EventArgs e)
+        {
+            string ip = textBox_ip.Text;
+
+            treeView1.Nodes.Clear();
+            treeView1.Nodes.Add(CreateDirectoryNode(ip, "Ftp Test"));
+        }
+
+        private TreeNode CreateDirectoryNode(string path, string name)
+        {
+            string user = textBox_user.Text;
+            string pass = textBox_pass.Text;
+            string ip = path;
+
+            TreeNode directoryNode = new TreeNode(name);
+            var fileList = GetFileList(user, pass, ip, "");
+
+            var directories = fileList.Where(f => f.IsDirectory);
+            var files = fileList.Where(f => !f.IsDirectory);
+
+            foreach(var dir in directories)
+            {
+                directoryNode.Nodes.Add(CreateDirectoryNode(dir.FullPath, dir.Name));
+            }
+            foreach(var file in files)
+            {
+                directoryNode.Nodes.Add(new TreeNode(file.Name));
+            }
+
+            return directoryNode;
+        }
+    }
+
+    public class FtpDetails
+    {
+        public bool IsDirectory
+        {
+            get
+            {
+                return Type.Equals("<DIR>");
+            }
+        }
+
+        internal string Type { get; set; }
+        public string Name { get; set; }
+        public string Time { get; set; }
+        public string Date { get; set; }
+
+        public string FullPath { get; set; }
+    }
+}
